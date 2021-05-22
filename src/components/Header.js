f60eaa4f1c65e6dfce36a6af0ec40597a7c688f1
sailorj94 <jackniall9@gmail.com>
2021-05-22 14:03:30 +0100

    Header postitions correctly

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import './Header.css'
+
+const Header = () => {
+
+    return (
+        <>
+            <h1 className = 'title' >Crisis Mapper</h1>
+        </>
+    )
+}
+
+export default Header;
