ddba03f74b182d87d1f0427a113eee7afe77fe43
sailorj94 <jackniall9@gmail.com>
2021-05-22 14:21:49 +0100

    Some styling changes to get text rendering in the foreground

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
     return (
         <>
             <h1 className = 'title' >Crisis Mapper</h1>
+            <h4 className = 'front-text'>Explore the world and discover the most recent major events.</h4>
         </>
     )
 }
