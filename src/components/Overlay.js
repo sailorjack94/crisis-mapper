70799b130889c7c2b025cbc866f1156834e896fa
sailorj94 <jackniall9@gmail.com>
2021-05-24 16:22:43 +0100

    Styling

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -8,7 +8,9 @@ const Overlay = ({eventTypes, onCrisisChange}) => {
             <h1>Crisis Mapper</h1>
             <div className="intro">
                 <p>
-                    Use our interactive app to view major geological and meteorological events from around the world
+                    <p>Use our interactive app to view major geological and meteorological events from around the world.</p>
+
+                    <p>Once you have selected a category, click on an event and more information will appear.</p>
                 </p>                
             </div>
             <CrisisSelector eventTypes={eventTypes} onCrisisChange={onCrisisChange}/>
