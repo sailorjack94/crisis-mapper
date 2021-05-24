4ebdf18a5b3150881b0cd86c8d645622196affaa
Spencer <Spencer.Taber@yahoo.com>
2021-05-24 11:14:01 +0100

    altitude configuration complete

--- src/containers/GlobeContainer.js
+++ src/containers/GlobeContainer.js
@@ -105,7 +105,7 @@ const GlobeContainer = () => {
                 pointLabel = {event => event.ev_region}
                 pointColor = {() => '#ff0000'}
                 pointRadius = {0.5}
-                pointAltitude = {event => normaliseLabels(event.ev_mag_value, events[events.length - 1].ev_mag_value, events[0].ev_mag_value, 0.01, 1.0)}
+                pointAltitude = {event => normaliseLabels(event.ev_mag_value, events[events.length - 1].ev_mag_value, events[0].ev_mag_value, 0.01, 0.5)}
                 pointResolution = {3}
                 //add callback function to display something. Callback event.
                 onPointClick = {event => zoomToPoint(event)}
