bc27fa9cdb6737de69e92ebe0cd52c03d99ba866
sailorj94 <jackniall9@gmail.com>
2021-05-25 12:50:53 +0100

    New Modal files added

---
+++ src/components/ModalContainer.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Modal from './modal/InfoModal';
+
+class ModalContainer extends Component {
+    constructor() {
+        super();
+        this.state = {
+            show: false
+        };
+        this.showModal = this.showModal.bind(this);
+        this.hideModal = this.hideModal.bind(this);
+    }
+
+    showModal = () => {
+        this.setState({ show: true });
+    };
+
+    hideModal = () => {
+        this.setState({ show: false });
+    };
+
+    render() {
+        return (
+            <main>
+                <Modal show={this.state.show} handleClose={this.hideModal}>
+                    <p>Modal</p>
+                </Modal>
+                <button type="button" onClick={this.showModal}>
+                    More Info
+        </button>
+            </main>
+        );
+    }
+}
+
+export default ModalContainer;
