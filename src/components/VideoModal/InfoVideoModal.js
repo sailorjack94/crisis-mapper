72339f82dc80d12c4a93c2a5fe6985ec7f271832
sailorj94 <jackniall9@gmail.com>
2021-05-26 10:40:53 +0100

    Changing to react-bootstrap

--- src/components/VideoModal/InfoVideoModal.js
+++ src/components/VideoModal/InfoVideoModal.js
@@ -3,9 +3,9 @@ import './InfoVideoModal.css';
 const VideoModal = ({ handleVideoClose, show, children }) => {
   const showHideVideoClassName = show ? "modal display-block" : "modal display-none";
 
-//  const endVideo = modal.on('hidden.bs.modal', function (e) {
-//     "#modal iframe".attr("src", ("#modal iframe").attr("src"));
-// });
+  //  const endVideo = modal.on('hidden.bs.modal', function (e) {
+  //     "#modal iframe".attr("src", ("#modal iframe").attr("src"));
+  // });
 
   return (
     <div className={showHideVideoClassName}>
@@ -14,58 +14,72 @@ const VideoModal = ({ handleVideoClose, show, children }) => {
           <h2>Videos</h2>
           <h4>Learn more about each of the natural disasters by watching the videos below!</h4>
 
-          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <div id="media-carousel" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              <li data-target="#media-carousel" data-slide-to="0" class="active"></li>
+              <li data-target="#media-carousel" data-slide-to="1"></li>
+              <li data-target="#media-carousel" data-slide-to="2"></li>
             </ol>
-
-
             <div class="carousel-inner">
+
+
               <div class="carousel-item active">
-                <iframe
-                  id="wildfire-video"
-                  class="d-block w-100"
-                  width="560"
-                  height="315"
-                  src="https://www.youtube.com/embed/5hghT1W33cY"
-                  title="YouTube video player"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen">
-                </iframe>
+                <div class="wrapper">
+                  <iframe
+                    width="315"
+                    height="315"
+                    src="https://www.youtube.com/embed/5hghT1W33cY"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen">
+                  </iframe>
+                </div>
               </div>
+
+
               <div class="carousel-item">
-                <iframe
-                  id="earthquake-video"
-                  class="d-block w-100"
-                  width="560"
-                  height="315"
-                  src="https://www.youtube.com/embed/e7ho6z32yyo"
-                  title="YouTube video player"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-                  show
-                  handleVideoClose
-                >
-                </iframe>
+                <div class="wrapper">
+                  <iframe
+                    width="315"
+                    height="315"
+                    src="https://www.youtube.com/embed/e7ho6z32yyo"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                    show
+                    handleVideoClose
+                  >
+                  </iframe>
+                </div>
               </div>
+
+
               <div class="carousel-item">
-                <iframe
-                  id="earthquake-video"
-                  class="d-block w-100"
-                  width="560"
-                  height="315"
-                  src="https://www.youtube.com/embed/e7ho6z32yyo"
-                  title="YouTube video player"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-                  show
-                  handleVideoClose
-                >
-                </iframe>
+                <div class="wrapper">
+                  <iframe
+                    width="315"
+                    height="315"
+                    src="https://www.youtube.com/embed/e7ho6z32yyo"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                    show
+                    handleVideoClose
+                  >
+                  </iframe>
+                </div>
               </div>
             </div>
+
+
+            <a class="carousel-control-prev" href="#media-carousel" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#media-carousel" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
           </div>
         </div>
 
