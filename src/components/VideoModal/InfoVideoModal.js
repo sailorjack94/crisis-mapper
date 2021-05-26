5c954f19b028ab79d07e0c19fd7ad07bddf73a39
Spencer <Spencer.Taber@yahoo.com>
2021-05-26 11:10:29 +0100

    adding updates

--- src/components/VideoModal/InfoVideoModal.js
+++ src/components/VideoModal/InfoVideoModal.js
@@ -10,32 +10,31 @@ const VideoModal = ({ handleVideoClose, show, children }) => {
 // });
 
   return (
-    <div>
-   <Carousel>
-           <Carousel.item>
-             
-             <div>
-                <iframe
-                  id="wildfire-video"
-                  class="d-block w-100"
-                  width="560"
+    <div className={showHideVideoClassName}>
+    <section className="modal-main-video">
+      <div className="modal-video">
+        <h2>Videos</h2>
+        <h4>Learn more about each of the natural disasters by watching the videos below!</h4>
+
+        <Carousel>
+          <Carousel.Item>
+          <iframe
+                  width="315"
                   height="315"
                   src="https://www.youtube.com/embed/5hghT1W33cY"
                   title="YouTube video player"
                   frameborder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen">
-                  </iframe>
-                  </div>
-                
-                 
-                 
-              </Carousel.Item>
-              <Carousel.item>
-              <div class="carousel-item">
-                <iframe
-                  id="earthquake-video"
-                  class="d-block w-100"
-                  width="560"
+                </iframe>
+            <Carousel.Caption>
+              <h3>Learn about something!</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+
+          <Carousel.Item>
+          <iframe
+                  width="315"
                   height="315"
                   src="https://www.youtube.com/embed/e7ho6z32yyo"
                   title="YouTube video player"
@@ -43,39 +42,47 @@ const VideoModal = ({ handleVideoClose, show, children }) => {
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
                 >
                 </iframe>
-              </div>
-              </Carousel.item>
-              <Carousel.item>
-              <div class="carousel-item">
-                <iframe
-                  id="earthquake-video"
-                  class="d-block w-100"
-                  width="560"
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+
+          <Carousel.Item>
+          <iframe
+                  width="315"
                   height="315"
                   src="https://www.youtube.com/embed/e7ho6z32yyo"
                   title="YouTube video player"
                   frameborder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-            
+                  show
+                  handleVideoClose
                 >
                 </iframe>
-                </div>
-                </Carousel.item>
-                </Carousel>
-                </div>
-            
-         
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
 
 
-        <br></br>
-        <button className="btn btn-outline-light" type="button" onClick={handleVideoClose}>
-          Close
-        </button>
-        <br></br>
-        <p>  </p>
-       
 
-  );
+      <br></br>
+      <button className="btn btn-outline-light" type="button" onClick={handleVideoClose}>
+        Close
+      </button>
+      <br></br>
+      <p>  </p>
+      </div>
+    </section>
+  </div>
+);
 };
+
+
 
 export default VideoModal;
