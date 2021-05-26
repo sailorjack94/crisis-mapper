1d44c9e2666edeef4fd02425f50d84f84a7cef04
sailorj94 <jackniall9@gmail.com>
2021-05-26 09:47:44 +0100

    Video carousel test feature

--- src/components/VideoModal/InfoVideoModal.js
+++ src/components/VideoModal/InfoVideoModal.js
@@ -10,49 +10,67 @@ const VideoModal = ({ handleVideoClose, show, children }) => {
   return (
     <div className={showHideVideoClassName}>
       <section className="modal-main-video">
-      <div className="modal-video">
-    <h2>Videos</h2>
-    <h4>Learn more about each of the natural disasters by watching the videos below!</h4>
+        <div className="modal-video">
+          <h2>Videos</h2>
+          <h4>Learn more about each of the natural disasters by watching the videos below!</h4>
 
-    <iframe 
-    id = "earthquake-video"
-    class = "video"
-    width="560" 
-    height="315" 
-    src="https://www.youtube.com/embed/e7ho6z32yyo" 
-    title="YouTube video player" 
-    frameborder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" 
-    show
-    handleVideoClose
-    >
-    </iframe>
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
 
-    <iframe 
-    id = "volcano-video"
-    class = "video"
-    width="560" 
-    height="315" 
-    src="https://www.youtube.com/embed/VNGUdObDoLk" 
-    title="YouTube video player" 
-    frameborder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-  >
-    </iframe>
 
-    <iframe
-    id = "wildfire-video"
-    class = "video"
-    width="560" 
-    height="315" 
-    src="https://www.youtube.com/embed/5hghT1W33cY" 
-    title="YouTube video player" 
-    frameborder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen">
-    </iframe>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <iframe
+                  id="wildfire-video"
+                  class="d-block w-100"
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/5hghT1W33cY"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen">
+                </iframe>
+              </div>
+              <div class="carousel-item">
+                <iframe
+                  id="earthquake-video"
+                  class="d-block w-100"
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/e7ho6z32yyo"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                  show
+                  handleVideoClose
+                >
+                </iframe>
+              </div>
+              <div class="carousel-item">
+                <iframe
+                  id="earthquake-video"
+                  class="d-block w-100"
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/e7ho6z32yyo"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                  show
+                  handleVideoClose
+                >
+                </iframe>
+              </div>
+            </div>
+          </div>
+        </div>
 
-      </div>
-      <br></br>
+
+        <br></br>
         <button className="btn btn-outline-light" type="button" onClick={handleVideoClose}>
           Close
         </button>
